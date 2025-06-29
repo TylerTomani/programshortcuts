d503b25419e3183bb6a45e8145d74c0b2238a003
shortcutter <shortcutter@MacBookPro.lan>
2025-06-29 17:29:55 -0400

    main

--- js/drag-hide.js
+++ js/drag-hide.js
@@ -1,27 +1,30 @@
 let startX = 0;
 let endX = 0;
 const sidebar = document.querySelector('.side-bar');
+if (window.innerWidth <= 768) {
+  // insert the swipe code here
 
-// Touch start
-document.addEventListener('touchstart', (e) => {
-  startX = e.touches[0].clientX;
-});
+    // Touch start
+    document.addEventListener('touchstart', (e) => {
+    startX = e.touches[0].clientX;
+    });
 
-// Touch move
-document.addEventListener('touchmove', (e) => {
-  endX = e.touches[0].clientX;
-});
+    // Touch move
+    document.addEventListener('touchmove', (e) => {
+    endX = e.touches[0].clientX;
+    });
 
-// Touch end
-document.addEventListener('touchend', () => {
-  const swipeDistance = startX - endX;
+    // Touch end
+    document.addEventListener('touchend', () => {
+    const swipeDistance = startX - endX;
 
-  // Detect left swipe (more than 50px)
-  if (swipeDistance > 50) {
-    sidebar.classList.add('hidden');
-  }
-  // Optionally, allow swipe right to show again
-  else if (swipeDistance < -50) {
-    sidebar.classList.remove('hidden');
-  }
-});
+    // Detect left swipe (more than 50px)
+    if (swipeDistance > 50) {
+        sidebar.classList.add('hidden');
+    }
+    // Optionally, allow swipe right to show again
+    else if (swipeDistance < -50) {
+        sidebar.classList.remove('hidden');
+    }
+    });
+}
