0beb59ddc6c1201f2bb32394a182b7075f68abb4
shortcutter <shortcutter@MacBookPro.lan>
2025-06-29 23:18:14 -0400

    main

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,9 +1,18 @@
-addEventListener('keydown', e => {
-    let key = e.key.toLowerCase()
-    if(key === 'k' && e.shiftKey && e.metaKey){
-        console.log('yes')
-        const body = document.querySelector('body')
-        body.classList.toggle('dark-mode')
-         
+
+
+
+  addEventListener('keydown', e => {
+    let key = e.key.toLowerCase();
+    if (key === 'k' && e.shiftKey && e.metaKey) {
+      console.log('yes');
+      const body = document.querySelector('body');
+      const pageContainer = document.querySelector('.page-container');
+      const mainShorcutPageContainer = document.getElementById('mainShorcutPageContainer');
+
+      mainShorcutPageContainer?.classList.toggle('dark-mode');
+      body.classList.toggle('dark-mode');
+      pageContainer?.classList.toggle('dark-mode');
+    } else {
+      console.log('no');
     }
-});
+  });
--- js/step-focus.js
+++ js/step-focus.js
@@ -6,13 +6,13 @@ export function stepFocus(){
         let key = e.key
         if(!isNaN(key)){
             const intlet = parseInt(key)
-            console.log(intlet)
+            // console.log(intlet)
             // stepTxts[intlet - 1].scrollIntoView({behavior: 'smooth', block: 'center'})
             if(stepTxts.length == 0) return
-            stepTxts[intlet - 1].focus()
+            stepTxts[intlet - 1]?.focus()
         }
         if(key.toLocaleLowerCase() === 'm' && e.target.id != 'mainShorcutPageContainer'){
-            console.log(mainShorcutPageContainer)
+            // console.log(mainShorcutPageContainer)
             body.scrollTo(0, 0)
     
 
