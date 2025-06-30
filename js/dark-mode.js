c49c8d6ae1d21c197775a3c8bca9f2ab65d89cfa
shortcutter <shortcutter@MacBookPro.lan>
2025-06-29 21:35:55 -0400

    main

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,6 +1,7 @@
 addEventListener('keydown', e => {
     let key = e.key.toLowerCase()
     if(key === 'k' && e.shiftKey && e.metaKey){
+        console.log('yes')
         const body = document.querySelector('body')
         body.classList.toggle('dark-mode')
          
